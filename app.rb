6826a415ec8e1389d0161d558b22e59c90136c2a
Joshua Olivares <joshuaolivares@utexas.edu>
2019-05-29 03:32:20 +0000

    update palindrome gem

--- app.rb
+++ app.rb
@@ -1,13 +1,22 @@
 require 'sinatra'
+require 'olive_palindrome'
 
 get '/' do
+  @title = 'Home'
   erb :index
 end
 
 get '/about' do
+  @title = 'About'
   erb :about
 end
 
 get '/palindrome' do
+  @title = 'Palindrome Detector'
   erb :palindrome #, :layout => :layout
+end
+
+post '/check' do
+  @phrase = params[:phrase]
+  erb :result
 end
--- test/site_pages_test.rb
+++ test/site_pages_test.rb
@@ -7,22 +7,35 @@ class PalindromeAppTest < Minitest::Test
     Sinatra::Application
   end
 
+  def setup
+    @base = "Learn Enough Ruby Sample App"
+  end
+  
   def test_index
     get '/'
     assert last_response.ok?
     #same as .css('h1').first
     assert doc(last_response).at_css('h1')
+    title = doc(last_response).at_css('title').content
+    assert_equal "#{@base} | Home", title
+    assert doc(last_response).at_css('nav')
+    assert  doc(last_response).at_css('.container > header > nav')
   end
 
   def test_about
     get '/about'
     assert last_response.ok?
     assert doc(last_response).at_css('h1')
+    title = doc(last_response).at_css('title').content
+    assert_equal "#{@base} | About", title
+    assert doc(last_response).at_css('head > link')
   end
 
   def test_palindrome
     get '/palindrome'
     assert last_response.ok?
     assert doc(last_response).at_css('h1')
+    title = doc(last_response).at_css('title').content
+    assert_equal "#{@base} | Palindrome Detector", title
   end
 end
